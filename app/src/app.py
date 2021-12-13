74cb629c1cd4d7709ae07deffe6021547e36ffd1
Branislav Goga <brano.goga@centrum.sk>
2021-12-13 20:30:52 +0100

    Add static template with Bootstrap layout

--- app/src/app.py
+++ app/src/app.py
@@ -1,13 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Flask: Hello World from Docker'
-
-@app.route('/api')
-def rest_hello_world():
-    return '{"id":1,"message":"Flask: Hello World from Docker"}'
+def tasks():
+    return render_template('tasks.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
