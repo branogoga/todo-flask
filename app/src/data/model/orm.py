076b623b4f40a1adccf9eb6dee010b7d21b39da5
Branislav Goga <brano.goga@centrum.sk>
2021-12-22 21:36:31 +0100

    Wire the DB and server on the Docker network

--- app/src/data/model/orm.py
+++ app/src/data/model/orm.py
@@ -1,11 +1,23 @@
+import logging
+import os
+
 from data.model.base import DeclarativeBase
 from data.model.task import Task  # noqa: F401
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
+logger = logging.getLogger(__name__)
+
+def get_db_uri() -> str:
+    db_uri = os.getenv('DATABASE_URL')
+    if not db_uri:
+        db_uri = "postgresql+pg8000://postgres:cicvor@localhost:5432/postgres"
+    logger.warning(db_uri)
+    return db_uri
+
 
 def create_db_engine():
-    engine = create_engine("postgresql+pg8000://postgres:cicvor@localhost:5432/postgres", connect_args={'timeout': 10})
+    engine = create_engine(get_db_uri(), connect_args={'timeout': 10})
     DeclarativeBase.metadata.create_all(engine, checkfirst=True)
     return engine
 
