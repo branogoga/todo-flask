b90d1d60401f3faafeb07bec1de4c0dea31cf354
Branislav Goga <brano.goga@centrum.sk>
2021-12-23 09:40:09 +0100

    Add task created at date

--- app/src/data/model/task.py
+++ app/src/data/model/task.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, func
 
 from data.model.base import DeclarativeBase
 
@@ -9,3 +9,4 @@ class Task(DeclarativeBase):
     id = Column(Integer, primary_key=True)
     title = Column(String)
     description = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
